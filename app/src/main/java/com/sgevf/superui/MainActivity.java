c7183efe61513c9f75123d1903a4c631e5c5414e
xunhuang <1007944735@qq.com>
2020-03-07 20:40:27 +0800

    2

--- app/src/main/java/com/sgevf/superui/MainActivity.java
+++ app/src/main/java/com/sgevf/superui/MainActivity.java
@@ -1,42 +1,72 @@
 package com.sgevf.superui;
 
-import android.app.Notification;
-import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.sgevf.ui.banner.ScrollingBanner;
-import com.sgevf.ui.banner.DefaultScrollingAdapter;
-import com.sgevf.ui.superRefreshView.SuperRefreshView;
-import com.sgevf.ui.tabView.CustomTabLayout;
-import com.sgevf.ui.utils.NotificationUtil;
+import com.sgevf.ui.horizontalScrollerView.HorizontalScrollerView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private SuperRefreshView superRefreshView;
-    private RecyclerView recyclerView;
-    private RecyclerViewAdapter recyclerViewAdapter;
+
+    private HorizontalScrollerView horizontalScroll;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        superRefreshView = findViewById(R.id.superRefreshView);
-        recyclerView = findViewById(R.id.recyclerView);
-        List<String> data = new ArrayList<>();
+        horizontalScroll = findViewById(R.id.horizontal_scroll);
+        List<String> list = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
-            data.add(i + "");
+            list.add(i + "");
         }
-        recyclerViewAdapter = new RecyclerViewAdapter(this, data);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(recyclerViewAdapter);
+        Adapter adapter = new Adapter(this, list);
+        horizontalScroll.setAdapter(adapter);
 
+    }
+
+
+    class Adapter extends RecyclerView.Adapter<VH> {
+        private Context context;
+        private List<String> datas;
+
+        public Adapter(Context context, List<String> datas) {
+            this.context = context;
+            this.datas = datas;
+        }
+
+        @NonNull
+        @Override
+        public VH onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return new VH(LayoutInflater.from(context).inflate(R.layout.item_scroll, null));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull VH vh, int i) {
+            vh.content.setText(datas.get(i));
+        }
+
+        @Override
+        public int getItemCount() {
+            return datas.size();
+        }
+    }
+
+    private class VH extends RecyclerView.ViewHolder {
+        private TextView content;
+
+        public VH(@NonNull View itemView) {
+            super(itemView);
+            content = itemView.findViewById(R.id.content);
+        }
     }
 }
---
+++ ui/src/main/java/com/sgevf/ui/horizontalScrollerView/HorizontalScrollerView.java
@@ -0,0 +1,61 @@
+package com.sgevf.ui.horizontalScrollerView;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+
+public class HorizontalScrollerView extends RecyclerView {
+    private LinearLayoutManager layoutManager;
+    private int offset;
+
+    public HorizontalScrollerView(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public HorizontalScrollerView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public HorizontalScrollerView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        setLayoutManager(layoutManager);
+        addOnScrollListener(mOnScrollListener);
+    }
+
+    private final OnScrollListener mOnScrollListener = new OnScrollListener() {
+        @Override
+        public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+            if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                smoothScrollBy(offset, 0, new DecelerateInterpolator());
+            }
+        }
+
+        @Override
+        public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+            int position = layoutManager.findFirstVisibleItemPosition();
+            View view = layoutManager.findViewByPosition(position);
+            int left = view.getLeft();
+            int width = view.getWidth();
+            if (Math.abs(left) <= width / 2) {
+                offset = left;
+            } else {
+                if (position == getChildCount() - 1) {
+                    offset = left;
+                } else {
+                    offset = width + left;
+                }
+            }
+        }
+    };
+}
